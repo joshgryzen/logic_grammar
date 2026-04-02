d63fd51f508bd56e1b6d302b8523cf1a18319da4
joshgryzen <cjgryzen@gmail.com>
2026-04-02 15:08:41 -0500

    cleaned

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -10,32 +10,6 @@ model = outlines.from_transformers(
     AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-3B-Instruct")
 )
 
-
-example_grammar = """
-    start: definition " " entity
-
-    definition: ("A" | "An") " " entity_name " has " attributes "."
-    entity_name: "movie" | "book" |
-    attributes: attribute ", " attribute | attribute
-    attribute: "id" | "time duration" | "title" | "author" |
-
-    entity: "a " name " with " entity_attributes "."
-    name: entity_name " M" | entity_name " B"
-    entity_attributes: attribute " " attribute_name ", with " entity_attributes | attribute " " attribute_name
-    attribute_name: "MovieID" | "BookID" | "BookTitle" | "BookAuthor" |
-"""
-
-example_prompt = """Your task is to summarize natural language input into a controlled sublanguage of english.
-
-
-          Input: Movies are organized by id and time duration. There is a movie with name M and id MovieID.
-          Output: A movie has id, time duration. a movie M with id MovieID.
-
-          Input: Books are organized by title and author. There is a book with name B that has the title BookTitle, and author BookAuthor.
-          Output:
-
-"""
-
 logic_grammar = """
 atom: "a" | "b" | "c"
 literal: "not "? atom 
@@ -46,9 +20,6 @@ fact: atom
 rule: fact | basic
 start: rule
 """
-
-# output_type = CFG(example_grammar)
-output_type = CFG(logic_grammar)
 
 logic_prompt = """Your task is to generate a valid answer set programming (ASP) rule.
 
@@ -64,6 +35,7 @@ logic_prompt = """Your task is to generate a valid answer set programming (ASP) 
 
 """
 
-grammar_output = model(logic_prompt, output_type)
-# grammar_output = model(example_prompt, output_type)
-print(grammar_output)
+output_type = CFG(logic_grammar)
+
+output = model(logic_prompt, output_type)
+print(output)
