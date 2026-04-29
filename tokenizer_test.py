57960898415b1e31c8d606f41498af891f639d2c
joshgryzen <cjgryzen@gmail.com>
2026-04-29 13:32:05 -0500

    fix

--- tokenizer_test.py
+++ tokenizer_test.py
@@ -1,6 +1,6 @@
 from transformers import AutoTokenizer, pipeline
 
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-8B-Instruct")
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
 
 print(tokenizer.tokenize("entailed"))
 print(tokenizer.tokenize("not entailed"))
@@ -10,7 +10,7 @@ print(tokenizer("entailed").input_ids)
 print(tokenizer("not entailed").input_ids)
 print(tokenizer("\n").input_ids)
 
-pipe = pipeline("text-generation", model="meta-llama/Llama-3.2-8B-Instruct")
+pipe = pipeline("text-generation", model="meta-llama/Llama-3.1-8B-Instruct")
 
 print(pipe.tokenizer("entailed"))
 print(pipe.tokenizer("not entailed"))
